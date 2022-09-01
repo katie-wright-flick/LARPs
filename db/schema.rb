c18f04cf89decf433650fa31292bebda4a546777
Katie Wright <katie.wright@flickelectric.co.nz>
2022-09-01 16:25:50 +1200

    create characters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_214401) do
+ActiveRecord::Schema.define(version: 2022_09_01_042440) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "job"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
